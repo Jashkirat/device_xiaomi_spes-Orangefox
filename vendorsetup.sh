38ddaa4d70c748a1263348e60879e0efe6475991
Jashkirat <jashkiratsingh@gmail.com>
2023-10-20 15:52:00 -0400

    spes: Create vendorsetup.sh script based on OrangeFox sources
    This commit introduces a new vendorsetup.sh script for the Spes device, inspired by the OrangeFox project.
    
    The script is created by combining configurations from the following sources:
    - [OrangeFox/device/vayu vendorsetup.sh](https://gitlab.com/OrangeFox/device/vayu/-/blob/fox_12.1/vendorsetup.sh)
    - [OrangeFox/vendor/recovery orangefox_build_vars.txt](https://gitlab.com/OrangeFox/vendor/recovery/-/blob/fox_12.1/orangefox_build_vars.txt)
    
    Signed-off-by: Jashkirat <jashkiratsingh@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,83 @@
+#
+#	This file is part of the OrangeFox Recovery Project
+# 	Copyright (C) 2018-2023 The OrangeFox Recovery Project
+#	
+#	OrangeFox is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	any later version.
+#
+#	OrangeFox is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+# 	This software is released under GPL version 3 or any later version.
+#	See <http://www.gnu.org/licenses/>.
+# 	
+# 	Please maintain this if you use this script or any part of it
+#
+FDEVICE="spes"
+fox_get_target_device() {
+local chkdev=$(echo "$BASH_SOURCE" | grep -w $FDEVICE)
+	if [ -n "$chkdev" ]; then
+		FOX_BUILD_DEVICE="$FDEVICE"
+	else
+		chkdev=$(set | grep BASH_ARGV | grep -w $FDEVICE)
+		[ -n "$chkdev" ] && FOX_BUILD_DEVICE="$FDEVICE"
+	fi
+}
+if [ -z "$1" -a -z "$FOX_BUILD_DEVICE" ]; then
+	fox_get_target_device
+fi
+
+if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
+	export FOX_TARGET_DEVICES="spes,spesn"
+	export TARGET_DEVICE_ALT="spesn"
+	export FOX_ENABLE_APP_MANAGER=1
+	export FOX_DELETE_AROMAFM=1
+    export FOX_USE_BASH_SHELL=1
+    export FOX_USE_NANO_EDITOR=1
+    export FOX_USE_TAR_BINARY=1
+	export FOX_USE_SED_BINARY=1
+	export FOX_USE_XZ_UTILS=1
+	export FOX_BUGGED_AOSP_ARB_WORKAROUND="1616300800"; # Sun 21 Mar 04:26:40 GMT 2021
+	export OF_QUICK_BACKUP_LIST="/boot;/data;"
+	export FOX_RECOVERY_SYSTEM_PARTITION="/dev/block/mapper/system"
+	export FOX_RECOVERY_VENDOR_PARTITION="/dev/block/mapper/vendor"
+	export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
+    export OF_NO_MIUI_PATCH_WARNING=1
+	export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
+	export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
+	export FOX_VERSION="R11.1"
+	export OF_MAINTAINER="JashkiratV"
+    export FOX_VIRTUAL_AB_DEVICE=1
+    export OF_ADVANCED_SECURITY=1
+    export OF_FLASHLIGHT_ENABLE=1
+
+
+    # screen settings
+	export OF_SCREEN_H=2400
+	export OF_STATUS_H=100
+	export OF_STATUS_INDENT_LEFT=48
+	export OF_STATUS_INDENT_RIGHT=48
+  	export OF_HIDE_NOTCH=1
+	export OF_CLOCK_POS=1
+    export OF_ALLOW_DISABLE_NAVBAR=0    
+
+    # maximum permissible splash image size (in kilobytes); do *NOT* increase!
+	export OF_SPLASH_MAX_SIZE=130
+
+	# let's see what are our build VARs
+	if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
+  	   export | grep "FOX" >> $FOX_BUILD_LOG_FILE
+  	   export | grep "OF_" >> $FOX_BUILD_LOG_FILE
+   	   export | grep "TARGET_" >> $FOX_BUILD_LOG_FILE
+  	   export | grep "TW_" >> $FOX_BUILD_LOG_FILE
+ 	fi
+else
+	if [ -z "$FOX_BUILD_DEVICE" -a -z "$BASH_SOURCE" ]; then
+		echo "I: This script requires bash. Not processing the $FDEVICE $(basename $0)"
+	fi
+fi
+#
