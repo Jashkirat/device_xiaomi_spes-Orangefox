af7dad7520512bbd64d3e1ade2e901cb9d2abe68
JassiV <jashkiratsingh@gmail.com>
2024-03-02 06:13:01 +0000

    spes:Fixed Build Vars for MIUI Version of OFOX

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -36,9 +36,9 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 	export TARGET_DEVICE_ALT="spesn"
 	export FOX_ENABLE_APP_MANAGER=1
 	export FOX_DELETE_AROMAFM=1
-    export FOX_USE_BASH_SHELL=1
-    export FOX_USE_NANO_EDITOR=1
-    export FOX_USE_TAR_BINARY=1
+        export FOX_USE_BASH_SHELL=1
+        export FOX_USE_NANO_EDITOR=1
+        export FOX_USE_TAR_BINARY=1
 	export FOX_USE_SED_BINARY=1
 	export FOX_USE_XZ_UTILS=1
 	export FOX_BUGGED_AOSP_ARB_WORKAROUND="1616300800"; # Sun 21 Mar 04:26:40 GMT 2021
@@ -46,27 +46,29 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 	export FOX_RECOVERY_SYSTEM_PARTITION="/dev/block/mapper/system"
 	export FOX_RECOVERY_VENDOR_PARTITION="/dev/block/mapper/vendor"
 	export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
-    export OF_NO_MIUI_PATCH_WARNING=1
+        export OF_NO_MIUI_PATCH_WARNING=1
 	export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
 	export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
 	export FOX_VERSION="R11.1"
+        export FOX_BUILD_TYPE="Beta"
 	export OF_MAINTAINER="JashkiratV"
-    export FOX_VIRTUAL_AB_DEVICE=1
-    export OF_ADVANCED_SECURITY=1
-    export OF_FLASHLIGHT_ENABLE=1
-
+	export OF_NO_SPLASH_CHANGE=1
+        export FOX_VIRTUAL_AB_DEVICE=1
+        export OF_ADVANCED_SECURITY=1
+        export OF_FLASHLIGHT_ENABLE=1
+  
 	# Use Magisk v26.3 for the magisk addon
-	export FOX_USE_SPECIFIC_MAGISK_ZIP="$PWD/device/xiaomi/spes/addon/Magisk.v26.3.zip"
-
-    # screen settings
+	export FOX_USE_SPECIFIC_MAGISK_ZIP="$device/xiaomi/spes/addon/Magisk.v26.3.zip"
+       
+        #  Screen settings
 	export OF_SCREEN_H=2400
 	export OF_STATUS_H=100
 	export OF_STATUS_INDENT_LEFT=48
 	export OF_STATUS_INDENT_RIGHT=48
   	export OF_HIDE_NOTCH=1
 	export OF_CLOCK_POS=1
-    export OF_ALLOW_DISABLE_NAVBAR=0    
-
+        export OF_ALLOW_DISABLE_NAVBAR=0    
+  
     # maximum permissible splash image size (in kilobytes); do *NOT* increase!
 	export OF_SPLASH_MAX_SIZE=130
 
