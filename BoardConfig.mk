355674045e8a4be9438b4a3467c44631a08ffb11
sayann70 <sayansarkarda2@gmail.com>
2026-05-31 18:34:06 +0000

    spes: Update kernel dir
    Signed-off-by: sayann70 <sayansarkarda2@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -71,8 +71,8 @@ BOARD_KERNEL_CMDLINE += swiotlb=2048
 
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_CONFIG := vendor/spes-perf_defconfig
-TARGET_KERNEL_HEADERS := kernel/xiaomi/sm6225
-TARGET_KERNEL_SOURCE := kernel/xiaomi/sm6225
+TARGET_KERNEL_HEADERS := kernel/xiaomi/spes
+TARGET_KERNEL_SOURCE := kernel/xiaomi/spes
 BOARD_RAMDISK_USE_LZ4 := true
 
 # Metadata
